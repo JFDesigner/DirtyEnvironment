eb4c1fab3f119c7d8988f238b5caeb86802b469f
Jonathan Flynn <jonflynndesigner@gmail.com>
2017-01-10 15:31:01 +0000

    initial commit

---
+++ DirtyEnvironment/DirtyEnv.cpp
@@ -0,0 +1,28 @@
+#include "windows.h"
+#include <iostream>
+
+static UINT const  broadcastTimeout = 10; // in milliseconds
+
+int main() 
+{
+	DWORD_PTR result;
+
+	// Broadcast WM_SETTINGCHANGE
+	SendMessageTimeout(HWND_BROADCAST,
+		WM_SETTINGCHANGE,
+		NULL,
+		reinterpret_cast<LPARAM>("Environment"),
+		SMTO_NORMAL,
+		broadcastTimeout,
+		&result);
+	if (result == 0)
+	{
+		std::cout << "Environment was successfully refreshed\n";
+	}
+	else
+	{
+		std::cerr << "Environment failed to refresh. Error Code: " << result << std::endl;
+	}
+
+	return static_cast<int>(result);
+}
